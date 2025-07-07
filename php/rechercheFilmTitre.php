8bd24915c50d468488c93b5db216d40dd479c6d3
oblerion <oblerion@protonmail.com>
2025-07-07 12:02:38 +0200

    recheche titre resume

--- php/rechercheFilmTitre.php
+++ php/rechercheFilmTitre.php
@@ -1,7 +1,7 @@
 <?php
-function rechercheFilmTitre($db, $atfind) // retourne le tableau de résultat
+function rechercheFilmTitreSynopsis($db, $atfind) // retourne le tableau de résultat
 {
-    $sql = "SELECT * FROM films WHERE films_titre LIKE(%$atfind%);";
+    $sql = "select * from films where films_titre like(".'%'.$atfind.'%'.") OR films_resume like(".'%'.$atfind.'%'.");";
     $instru = $db->prepare($sql);
     $instru->execute();
     $instru->SetFetchMode(PDO::FETCH_ASSOC);
